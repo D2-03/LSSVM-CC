22b1524d765088394675c3394d679cf47daaafa0
CandyT <67602914+D2-03@users.noreply.github.com>
2023-06-10 14:56:40 +0800

    Delete func directory

--- func/checkIllest.m
+++
@@ -1,9 +0,0 @@
-function checkIllest(ill)
-% ill=est_ill_nowht;
-% checkIllest(ill)
-minVal=min(min(ill,[],2));
-if minVal<=0 
-    disp(['There is negative value: ' minVal]);
-else
-    disp('The values are positive.');
-end
